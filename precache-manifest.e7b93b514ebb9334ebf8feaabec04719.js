a24c3bf2417506d66a24985f367f3c64e4328c8d
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-08-22 17:35:48 +0500

    Updates

---
+++ precache-manifest.e7b93b514ebb9334ebf8feaabec04719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b860edd658cc71783de440e0b0f246ec",
+    "url": "/object-detector/index.html"
+  },
+  {
+    "revision": "fc4b56fd1a989be4f716",
+    "url": "/object-detector/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "32cc89206a1e0cc30da5",
+    "url": "/object-detector/static/js/2.27b1a25c.chunk.js"
+  },
+  {
+    "revision": "bf29194cb7566ef00dd8f7c7849cee11",
+    "url": "/object-detector/static/js/2.27b1a25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4b56fd1a989be4f716",
+    "url": "/object-detector/static/js/main.5ea3fca7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/object-detector/static/js/main.5ea3fca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6aa05968e4773958ca",
+    "url": "/object-detector/static/js/runtime-main.82dbcded.js"
+  }
+]);
